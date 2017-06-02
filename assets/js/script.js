bd55892edbace7bfb60e407ca9ae90412abed76e
Jonathan Costa <jonjcosta@gmail.com>
2017-06-02 19:19:50 -0300

    - Arranged main button's style and position - Added function to properly toggle temperature unit - Added Font Awesome lib for icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,43 @@
 $(function() {
     
-    var TEMP = 0;
+    // Tempreature Object that will hold the value and its metric
+    var TEMP = {
+        mode: "C",
+        val: 0
+    };
 
-    $(".jumbo__button").click(function(event) {
-        // event.preventDefault();
+    // Initiates the main function
+    $(".jumbo__button").click(function() {
+        var $this = $(this);
+        $this.button('loading');
         getLocation();
         $("body").css("background-image", 'url("https://s6.postimg.org/xupiuz03j/bg_rainy.jpg")');
     });
 
+    // Toggles the temperature unit between Celius and Fahrenheit
     $(".weather__button").click(function() {
-        TEMP = (TEMP*9/5) + 32;
-        $(".weather__temperature").html(TEMP + "F");
+        if (TEMP.mode == 'C') {
+            TEMP.mode = 'F';
+            TEMP.val = (TEMP.val * 9/5) + 32;
+            $(".temperature__val").html(TEMP.val +"F");
+        } else {
+            TEMP.mode = 'C';
+            TEMP.val = (TEMP.val - 32) * 5/9;
+            $(".temperature__val").html(TEMP.val +"°C");            
+        }
     });
     
+    // Obtains the user's location through the browser
     function getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(getWeather);
         } else {
             $(".txtTemp").html("Geolocation is not supported by this browser.");
         }
     }
 
-    function showPosition(position) {
+    // Uses the user's coordinates to send a request to the weather API
+    function getWeather(position) {
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather",
             data: {
@@ -30,14 +46,16 @@ $(function() {
                 APPID: "6bcdb2f0785621d3a6c531e0caf57c5a"
             },
             success: function(data) {
-                console.log(data);
+                // console.log(data);
                 $(".weather__main").html(data.weather[0].main);
                 $(".weather__local").html(data.name+ " - "+ data.sys.country);
-                TEMP = Math.round(data.main.temp - 273.15);
-                $(".weather__temperature").html(TEMP +"° C");
+                // The temperature comes in Kelvin, so we convert to Celsius before showing
+                TEMP.val = Math.round(data.main.temp - 273.15);
+                $(".temperature__val").html(TEMP.val +"° C");
                 $(".weather__wind").html(data.wind.speed);
                 $(".weather__humidity").html(data.main.humidity);
                 $(".weather").show();
+                $(".jumbo__button").button('reset');
             },
             error: function() {
                 console.log("Couldn't find the data.");
